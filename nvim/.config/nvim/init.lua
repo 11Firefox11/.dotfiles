6fcc3770f36fe2453b7552f6bf837b84a1453268
11Firefox11 <65167877+11Firefox11@users.noreply.github.com>
2024-06-26 12:48:52 +0200

    autosave nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -782,6 +782,12 @@ require('lazy').setup({
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
+  {
+    'okuuva/auto-save.nvim',
+    cmd = 'ASToggle',
+    event = { 'InsertLeave', 'TextChanged' }, -- optional for lazy loading on trigger events
+    opts = {},
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
